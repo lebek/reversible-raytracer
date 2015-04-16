disable gradient descent until 0.7.0 fixed

grad_descent.py
@@ -23,49 +23,11 @@ class GDOptimizer:
         Maximize the loss function with learning rate lr.
         """
 
-        render_fn = theano.function([], self.image,
-                                    on_unused_input='ignore',
-                                    allow_input_downcast=True)
-
-        # grad_fns = []
-        # for idx, var in enumerate(self.vars):
-        #     if self.locked(var):
-        #         grad_fns.append([])
-        #         continue
-        #     grad_fns.append(
-        #         theano.function(self.vars, T.grad(loss, var),
-        #                         on_unused_input='ignore',
-        #                         allow_input_downcast=True))
-
-        update_vars=[]
+        update_vars = []
         grads = T.grad(loss, self.vars)
         for var, gvar in zip(self.vars, grads):
             if self.locked(var): continue
             #if 'center' not in var: continue
             update_vars.append((var, var - lr * gvar))
 
-        train = theano.function([], loss, updates=update_vars)
-        return train, render_fn
-
-
-
-        # prev_updates = [0] * len(self.vars)
-        # for step in range(maxIterations):
-        #     for idx, var in enumerate(self.vars):
-        #         if self.locked(var): continue
-        #         grad = grad_fns[idx](*self.vals)
-        #         if np.isnan(grad).any():
-        #             print "nan grad"
-        #             continue
-        #         print var.name, grad
-        #         update = (lr * grad) + (momentum * prev_updates[idx])
-        #         self.vals[idx] = self.vals[idx] + update
-        #         self.vals[idx] = np.array(self.vals[idx]).clip(
-        #             var.low_bound, var.up_bound)
-
-        #         prev_updates[idx] = update
-
-        #         print self.vals[idx]
-        #         print ""
-
-        #     yield render_fn()
+        return theano.function([], loss, updates=update_vars)

scenemaker.py
@@ -95,7 +95,7 @@ class Scene:
         sampleDist_x = np.random.random((self.camera.x_dims, self.camera.y_dims,antialias_samples))
         sampleDist_y = np.random.random((self.camera.x_dims, self.camera.y_dims,antialias_samples))
 
-        for sample in xrange(antialias_samples): #TODO USE SCAN 
+        for sample in xrange(antialias_samples): #TODO USE SCAN
 
             #Make Rays
             self.camera.rays = self.camera.make_rays(self.camera.x_dims, self.camera.y_dims,\
@@ -217,9 +217,9 @@ class Camera:
         rays = np.dstack([np.ones([y_dims, x_dims]), rays])
         rays = np.divide(rays, np.linalg.norm(rays, axis=2).reshape(
                                         y_dims, x_dims, 1).repeat(3, 2))
-        
-        if sampleDist_x is not None: rays[:,:,1] = rays[:,:,1] + sampleDist_x / x_dims 
-        if sampleDist_y is not None: rays[:,:,2] = rays[:,:,2] + sampleDist_y / y_dims 
+
+        if sampleDist_x is not None: rays[:,:,1] = rays[:,:,1] + sampleDist_x / x_dims
+        if sampleDist_y is not None: rays[:,:,2] = rays[:,:,2] + sampleDist_y / y_dims
         return RayField('ray field', self.position, rays, x_dims, y_dims)
 
 
@@ -272,4 +272,3 @@ def simple_scene():
     shader = PhongShader('shader')
     #shader = DepthMapShader('shader')
     return Scene(objs, [light], camera, shader)
-