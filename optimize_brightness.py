rotation and square fixed. scene_setup file added

optimize_brightness.py
@@ -4,16 +4,19 @@ from scenemaker import simple_scene
 from grad_descent import GDOptimizer
 import theano
 from util import *
+from scene_setup import *
+
+### Hyper-parameters ###
+OptmizeFlag=False
+#-----------------------
+
 
 if not os.path.exists('output'):
     os.makedirs('output')
 
 scene = simple_scene()
-#scene.translate(scene.objects[2], (0.,2,7))
-scene.translate(scene.objects[1], (6.,1,1))
-scene.translate(scene.objects[0], (10,-1,1))
-scene.scale(scene.objects[0], (1,2,1.5), np.zeros((3,)))
-#scene.rotate(scene.objects[2], 'x', 90)
+scene_setup1(scene)
+
 
 opt = GDOptimizer(scene)
 
@@ -23,11 +26,18 @@ render_fn = theano.function([], image, on_unused_input='ignore')
 
 drawWithMarkers('output/0.png', render_fn())
 
-print 'Building gradient functions'
-train = opt.optimize(-image[90, 85].sum()-image[50, 90].sum(),
-                     0.0008, 0.1)
 
-for i in range(90):
-    print 'Step', i+1
-    print train()
-    drawWithMarkers('output/%d.png' % (i+1,), render_fn())
+if OptmizeFlag:
+    print 'Building gradient functions'
+    train = opt.optimize(-image[90, 85].sum()-image[50, 90].sum(),
+                         0.0008, 0.1)
+    
+    
+    for i in range(90):
+        print 'Step', i+1
+        print train()
+        drawWithMarkers('output/%d.png' % (i+1,), render_fn())
+
+
+
+

scene_object.py
@@ -49,25 +49,27 @@ class UnitSquare(SceneObject):
         self.variables.add_child(material.variables)
         #self.transform = self.variables.add(np.eye((3)), 'modelToWorldProj')
         #self.translate = self.variables.add(np.zeros((3,)), 'modelToWorldTrans')
-        self.invtransform = self.variables.add(np.eye((3)), 'worldToModelProj')
-        self.invtranslate = self.variables.add(np.zeros((3,)), 'worldToModelTrans')
+        self.invtransform = self.variables.add(np.eye((3), dtype='float32'), 'worldToModelProj')
+        self.invtranslate = self.variables.add(np.zeros((3,), dtype='float32'), 'worldToModelTrans')
 
     def _hit(self, rays, origin):
 
 
         mask_not_parallel_xy_plane = T.neq(rays[:,:,2],0)
-        ts = -ray_field.origin[2] / rays[:,:,2] #t is the
+        ts = -origin[2] / rays[:,:,2] #t is the
         mask_positive_t = T.gt(ts, 0)
-        intersection = ray_field.origin + ts.dimshuffle(0, 1, 'x')* rays
-        mask_interior_of_unitsquare = T.gt(intersection, -0.5) * T.lt(intersection,0.5)
-        mask = mask_interior_of_unitsquare * mask_positive_t.dimshuffle(0,1,'x')\
-                        * mask_not_parallel_xy_plane.dimshuffle(0,1,'x')
+        intersection = origin + ts.dimshuffle(0, 1, 'x')* rays
+        mask_interior_of_unitsquare_x = T.gt(intersection[:,:,0], -0.5) * T.lt(intersection[:,:,0],0.5)
+        mask_interior_of_unitsquare_y = T.gt(intersection[:,:,1], -0.5) * T.lt(intersection[:,:,1],0.5)
+        mask_interior_of_unitsquare = mask_interior_of_unitsquare_x * mask_interior_of_unitsquare_y
+        mask = mask_interior_of_unitsquare * mask_positive_t\
+                        * mask_not_parallel_xy_plane
 
         all_falses = (1-mask)
-        intersection = T.switch(all_falses, float('inf'), intersection)
-        intersection = T.set_subtensor(intersection[:,:,2], T.zeros_like(rays[:,:,2]))
-        intersection = T.dot(self.trans, intersection)
-        return mask, intersection,ts
+
+        ts = ts * mask
+        ts = T.switch(1-mask, float('inf'), ts)
+        return mask, ts
 
 
     def distance(self, ray_field):
@@ -75,26 +77,26 @@ class UnitSquare(SceneObject):
         """Returns the distances along the rays that hits occur.
         If no hit, returns inf."""
 
-        rays = T.dot(self.invtransform, ray_field.rays)
-        origin = T.dot(self.invtransform, ray_field.origin)  + self.invtranslate
-        mask, intersection, ts = self._hit(ray, origin)
-
+        origin  = T.dot(self.invtransform, ray_field.origin) + self.invtranslate
+        rays    = T.dot(ray_field.rays, self.invtransform.T)
+        mask, ts = self._hit(rays, origin)
         return ts #intersection
 
     def normals(self, ray_field):
+        
+        origin  = T.dot(self.invtransform, ray_field.origin) + self.invtranslate
+        rays    = T.dot(ray_field.rays, self.invtransform.T)
+        mask, ts = self._hit(rays, origin)
+        mask_positive_t = T.gt(origin[2], 0)
 
-        mask, intersection,ts = self._hit(ray_field)
-        mask_positive_t = T.gt(ts, 0)
-
-        pos_norm = np.tile( np.asarray([0.0,0.0,1.0]), \
+        pos_norm = np.tile( np.asarray([0.0,0.0,1.0], dtype='float32'), \
                  [ray_field.x_dims,ray_field.y_dims,1])
-        neg_norm = np.tile( np.asarray([0.0,0.0,-1.0]), \
+        neg_norm = np.tile( np.asarray([0.0,0.0,-1.0], dtype='float32'), \
                  [ray_field.x_dims,ray_field.y_dims,1])
 
-        norm = pos_norm * mask_positive_t.dimshuffle(0,1,'x') \
-                            + neg_norm * ( 1-mask_positive_t).dimshuffle(0,1,'x')
-        norm = norm * mask
-
+        norm = pos_norm * mask_positive_t\
+                            + neg_norm * ( 1-mask_positive_t)
+        norm = norm * mask.dimshuffle(0,1,'x')
         return transNorm(self.invtransform, norm)
 
 

scenemaker.py
@@ -138,7 +138,10 @@ class Scene:
         translate[0] = -trans[0];
         translate[1] = -trans[1];
         translate[2] = -trans[2];
-        sceneobject.invtranslate.set_value(translate)
+        #sceneobject.invtransform.set_value(translate + sceneobject.invtransform.get_value())
+        sceneobject.invtranslate.set_value(translate + \
+                        sceneobject.invtranslate.get_value())
+
 
     def scale(self, sceneobject, sc, origin):
 
@@ -163,21 +166,21 @@ class Scene:
         for i in xrange(2):
             if axis=='x':
                 rotateM[0,0] = 1;
-                rotateM[1,1] = np.cos(angle*toRadian);
+                rotateM[1,1] = np.cos (angle*toRadian);
                 rotateM[1,2] = -np.sin(angle*toRadian);
-                rotateM[2,1] = np.sin(angle*toRadian);
-                rotateM[2,2] = np.cos(angle*toRadian);
+                rotateM[2,1] = np.sin (angle*toRadian);
+                rotateM[2,2] = np.cos (angle*toRadian);
             elif axis=='y':
                 rotateM[0,0] = np.cos(angle*toRadian);
-                rotateM[1,1] = np.sin(angle*toRadian);
-                rotateM[1,2] = 1;
-                rotateM[2,1] = -np.sin(angle*toRadian);
+                rotateM[0,2] = np.sin(angle*toRadian);
+                rotateM[1,1] = 1;
+                rotateM[2,0] = -np.sin(angle*toRadian);
                 rotateM[2,2] = np.cos(angle*toRadian);
             elif axis=='z':
-                rotateM[0,0] = np.cos(angle*toRadian);
-                rotateM[1,1] = -np.sin(angle*toRadian);
-                rotateM[1,2] = np.sin(angle*toRadian)
-                rotateM[2,1] = np.cos(angle*toRadian);
+                rotateM[0,0] =  np.cos(angle*toRadian);
+                rotateM[0,1] = -np.sin(angle*toRadian);
+                rotateM[1,0] =  np.sin(angle*toRadian)
+                rotateM[1,1] =  np.cos(angle*toRadian);
                 rotateM[2,2] = 1
 
             if i == 0:
@@ -186,7 +189,7 @@ class Scene:
             else:
                 sceneobject.invtransform.set_value(np.dot(rotateM, sceneobject.invtransform.get_value()))
                 sceneobject.invtranslate.set_value(np.dot(rotateM, \
-                                sceneobject.invtranslate.get_value())+ rotateT)
+                                sceneobject.invtranslate.get_value()))
 
 
 
@@ -201,10 +204,10 @@ class RayField:
         self.y_dims = y_dims
 
 class Camera:
-    def __init__(self, name, position, look_at, x_dims, y_dims):
+    def __init__(self, name, position, x_dims, y_dims):
         self.variables = VariableSet(name)
         self.position = self.variables.add(position, 'position')
-        self.look_at = look_at
+        self.look_at = np.asarray([0,0,1.], dtype='float32')
         self.x_dims = x_dims
         self.y_dims = y_dims
 
@@ -212,14 +215,15 @@ class Camera:
         # this should be rewritten in theano - currently we can't do any
         # sensible optimization on camera parameters since we're calculating
         # the ray field prior to entering theano (thus losing parameterisation)
+        
         rays = np.dstack(np.meshgrid(np.linspace(0.5, -0.5, y_dims),
                          np.linspace(-0.5, 0.5, x_dims), indexing='ij'))
-        rays = np.dstack([np.ones([y_dims, x_dims]), rays])
+        rays = np.dstack([rays, np.ones([y_dims, x_dims])])
         rays = np.divide(rays, np.linalg.norm(rays, axis=2).reshape(
                                         y_dims, x_dims, 1).repeat(3, 2))
 
-        if sampleDist_x is not None: rays[:,:,1] = rays[:,:,1] + sampleDist_x / x_dims
-        if sampleDist_y is not None: rays[:,:,2] = rays[:,:,2] + sampleDist_y / y_dims
+        if sampleDist_x is not None: rays[:,:,0] = rays[:,:,0] + sampleDist_x / x_dims
+        if sampleDist_y is not None: rays[:,:,1] = rays[:,:,1] + sampleDist_y / y_dims
         return RayField('ray field', self.position, rays, x_dims, y_dims)
 
 
@@ -264,11 +268,14 @@ def simple_scene():
         Sphere('sphere 2', material2),
         #Sphere('sphere 3', material3),
         #Sphere('sphere 3', (5., -1., 1.), 1., material3)
-        #UnitSquare('square 1', material2)
+        UnitSquare('square 1', material3)
     ]
 
-    light = Light('light', (2., -1., -1.), (0.87, 0.961, 1.))
-    camera = Camera('camera', (0., 0., 0.), (1., 0., 0.), 128, 128)
+    light = Light('light', (-1., -1., 2.), (1., 0.87, 0.961))
+    #camera = Camera('camera', (0., 0., 0.), (1., 0., 0.), 128, 128)
+    camera = Camera('camera', (0., 0., 0.), 128, 128)
     shader = PhongShader('shader')
     #shader = DepthMapShader('shader')
     return Scene(objs, [light], camera, shader)
+
+