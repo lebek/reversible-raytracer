0beee5d21dea8587c1a52cf21a960fe334539182
Peter Le Bek <peteralebek@gmail.com>
2015-06-10 13:18:18 -0400

    Fix optimize brightness, remove scene setup

--- examples/optimize_brightness.py
+++ examples/optimize_brightness.py
@@ -6,14 +6,33 @@ from util import *
 from scene_setup import *
 
 ### Hyper-parameters ###
-OptmizeFlag=False
+OptmizeFlag=True
 #-----------------------
 
 
 if not os.path.exists('output'):
     os.makedirs('output')
 
-scene = scene2()
+center1 = theano.shared(np.asarray([-.5, -.5, 4], dtype=theano.config.floatX),
+                       borrow=True)
+center2 = theano.shared(np.asarray([.5, .5, 4], dtype=theano.config.floatX),
+                       borrow=True)
+
+material1 = Material((0.2, 0.9, 0.4), 0.3, 0.7, 0.5, 50.)
+material2 = Material((0.87, 0.1, 0.507), 0.3, 0.9, 0.4, 50.)
+
+t1 = translate(center1)
+t2 = translate(center2) * rotate(90, (0, 0, 1)) * scale((1, 2, 1.5))
+
+shapes = [
+    Sphere(t1, material1),
+    Sphere(t2, material2)
+]
+
+light = Light((-1., -1., 2.), (0.961, 1., 0.87))
+camera = Camera(128, 128)
+shader = PhongShader()
+scene = Scene(shapes, [light], camera, shader)
 
 opt = GDOptimizer()
 
@@ -23,11 +42,11 @@ render_fn = theano.function([], image, on_unused_input='ignore')
 
 drawWithMarkers('output/0.png', render_fn())
 
-
 if OptmizeFlag:
     print 'Building gradient functions'
-    train = opt.optimize(-image[90, 85].sum()-image[50, 90].sum(),
-                         0.0008, 0.1)
+    train = opt.optimize([center1, center2],
+        -image[90, 85].sum()-image[50, 90].sum(),
+        0.0008, 0.1)
 
 
     for i in range(90):
