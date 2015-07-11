fixed actiavtion units and better intialization tricks

autoencoder.py
@@ -45,20 +45,6 @@ class Autoencoder():
             params += self.capsules[i].params
         return params
 
-    def init_capsule_param(self, n_hidden_l3):
-        l3_to_center = 0.07*np.asarray(
-            np.random.uniform(
-                low=-4 * np.sqrt(6. / 6+n_hidden_l3),
-                high=4 * np.sqrt(6. / 6+n_hidden_l3),
-                size=(n_hidden_l3, 3)
-            ), dtype=theano.config.floatX)
-        l3_to_radius = 0.0007*np.asarray(
-            np.random.uniform(
-                low=-4 * np.sqrt(6. / 6+n_hidden_l3),
-                high=4 * np.sqrt(6. / 6+n_hidden_l3),
-                size=(n_hidden_l3, 3)
-            ), dtype=theano.config.floatX)
-        return np.concatenate((l3_to_center, l3_to_radius), 1)
 
     def get_reconstruct(self,X):
         robjs = self.encoder(X)
@@ -66,9 +52,9 @@ class Autoencoder():
 
     def encoder(self, X):
 
-        h1 = T.nnet.sigmoid(T.dot(X , self.W0) + self.l1_biases)
-        h2 = T.nnet.sigmoid(T.dot(h1, self.W1) + self.l2_biases)
-        h3 = T.nnet.sigmoid(T.dot(h2, self.W2) + self.l3_biases)
+        h1 = T.tanh(T.dot(X , self.W0) + self.l1_biases)
+        h2 = T.tanh(T.dot(h1, self.W1) + self.l2_biases)
+        h3 = T.nnet.softplus(T.dot(h2, self.W2) + self.l3_biases)
 
         rvars = []
         #TODO For loop needs to be replaced with scan to make it faster

autoencoder_1obj.py
@@ -35,6 +35,11 @@ class Autoencoder_1obj():
         self.params= self.params0+self.params1
 
     def init_capsule_param(self, n_hidden_l3):
+        #return 0.07*np.asarray( np.random.uniform(\
+        #            low=-4 * np.sqrt(6. / 6+n_hidden_l3),
+        #            high=4 * np.sqrt(6. / 6+n_hidden_l3),
+        #            size=(n_hidden_l3, 6)), dtype=theano.config.floatX)
+
         l3_to_center = 0.07*np.asarray(
             np.random.uniform(
                 low=-4 * np.sqrt(6. / 6+n_hidden_l3),
@@ -55,9 +60,9 @@ class Autoencoder_1obj():
 
     def encoder(self, X):
 
-        h1 = T.nnet.sigmoid(T.dot(X, self.vis_to_l1) + self.l1_biases)
-        h2 = T.nnet.sigmoid(T.dot(h1, self.l1_to_l2) + self.l2_biases)
-        h3 = T.nnet.sigmoid(T.dot(h2, self.l2_to_l3) + self.l3_biases)
+        h1 = T.tanh(T.dot(X, self.vis_to_l1) + self.l1_biases)
+        h2 = T.tanh(T.dot(h1, self.l1_to_l2) + self.l2_biases)
+        h3 = T.nnet.softplus(T.dot(h2, self.l2_to_l3) + self.l3_biases)
         rvar1 = T.dot(h3, self.l3_to_rvar1) + self.rvar1_biases
         rvar1 = T.set_subtensor(rvar1[3:], rvar1[3:].clip(0, np.inf))
         return rvar1

capsule.py
@@ -7,18 +7,32 @@ class Capsule():
     def __init__(self, name, n_hidden, n_output):
 
         self.name = name
-        self.params = [self.init_capsule_weight(n_hidden, n_output),
+        self.params = [self.init_capsule_weight(n_hidden),
             theano.shared(np.asarray([0,0,2.5,1,1,1], dtype=theano.config.floatX), borrow=True)]
 
 
-    def init_capsule_weight(self, n_hidden_l3, n_output):    
+    def init_capsule_weight(self, n_hidden_l3):    
 
-        return theano.shared(0.07*np.asarray(
-                np.random.uniform(
-                    low=-4 * np.sqrt(6. / n_output+n_hidden_l3),
-                    high=4 * np.sqrt(6. / n_output+n_hidden_l3),
-                    size=(n_hidden_l3, n_output)
-                ), dtype=theano.config.floatX),borrow=True)
+        l3_to_center = 0.05*np.asarray(
+            np.random.uniform(
+                low=-4 * np.sqrt(6. / 6+n_hidden_l3),
+                high=4 * np.sqrt(6. / 6+n_hidden_l3),
+                size=(n_hidden_l3, 3)
+            ), dtype=theano.config.floatX)
+        l3_to_radius = 0.0005*np.asarray( np.random.uniform(
+                low=-4 * np.sqrt(6. / 6+n_hidden_l3),
+                high=4 * np.sqrt(6. / 6+n_hidden_l3),
+                size=(n_hidden_l3, 3)
+            ), dtype=theano.config.floatX)
+        return theano.shared(np.concatenate((l3_to_center, l3_to_radius), 1))
+
+
+        #return theano.shared(0.07*np.asarray(
+        #        np.random.uniform(
+        #            low=-4 * np.sqrt(6. / n_output+n_hidden_l3),
+        #            high=4 * np.sqrt(6. / n_output+n_hidden_l3),
+        #            size=(n_hidden_l3, n_output)
+        #        ), dtype=theano.config.floatX),borrow=True)
 
 
 